de109ff523d3cf04a557796593ee5fb3cca7009f
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-07-06 12:33:34 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+valor = gets.chomp.to_i
+
+#p valor.class
+
+if valor > 20
+  puts "Esse valor é maior que 20"
+end
+
+
+# ex2 
+
+puts "Digitar um valor: "
+valor = gets.chomp.to_i
+  if valor == 0
+    puts "Valor é zero"
+  end
